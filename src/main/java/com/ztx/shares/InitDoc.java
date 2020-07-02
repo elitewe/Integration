05c59bf9bf9576b540cb082b8349db414307686e
171250611 <171250611@smail.nju.edu.cn>
2020-07-02 23:11:23 +0800

    untrack .idea/

--- src/main/java/com/ztx/shares/InitDoc.java
+++ src/main/java/com/ztx/shares/InitDoc.java
@@ -8,7 +8,7 @@ public class InitDoc {
     public static void main(String[] args) {
         DocsConfig config = new DocsConfig();
         config.setProjectPath("C:\\Users\\computer\\Desktop\\StockSpark"); // 项目根目录
-//        config.setProjectName("ProjectName"); // 项目名称
+//        config.setProjectName("ProjectName"); //  项目名称
         config.setApiVersion("V1.0");       // 声明该API的版本
         config.setDocsPath("C:\\Users\\computer\\Desktop\\StockSpark\\apidocs"); // 生成API 文档所在目录
         config.setAutoGenerate(Boolean.TRUE);  // 配置自动生成
