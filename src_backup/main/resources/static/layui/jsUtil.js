54307fe54832d7cbcfd141f2ad96527997f2a51b
咖啡馆大叔 <oncwnuKWjIFGR8CD0jinmRkRebrs@git.weixin.qq.com>
2020-07-02 23:17:34 +0800

    delete src_backup

--- src_backup/main/resources/static/layui/jsUtil.js
+++
@@ -1,7 +0,0 @@
-function autoScreen() {
-    var bodyHeight=$(document.body).height();
-    if(bodyHeight<625){
-        var tops=625-bodyHeight;
-        $(".layui-footer").css("marginTop",tops+"px");
-    }
-}
